88191b43727550cf75d69a8407ee2c51db55a0c2
Dave Connelly <dave@trongate.io>
2026-05-05 10:35:02 +0100

    Correction to DEFAULT_MODULE and DEFAULT_METHOD.

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 //The main config file
 define('BASE_URL', '****');
 define('ENV', 'dev');
-define('DEFAULT_MODULE', 'welcome');
-define('DEFAULT_METHOD', 'setup');
+define('DEFAULT_MODULE', 'setup');
+define('DEFAULT_METHOD', 'index');
 define('MODULE_ASSETS_TRIGGER', '_module');
 define('ERROR_404', 'templates/error_404');
